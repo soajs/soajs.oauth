44a95cc459dc033cd09c11e9445e8a4e1cf21c79
mikehajj <mike.hajj@gmail.com>
2015-03-20 00:24:20 +0200

    changed rest-proxy to 127.0.0.1

--- test/integration/soajs.oauth.test.js
+++ test/integration/soajs.oauth.test.js
@@ -18,7 +18,7 @@ var mongo = new Mongo(dashboardConfig);
 var extKey = 'aa39b5490c4a4ed0e56d7ec1232a428f771e8bb83cfcee16de14f735d0f5da587d5968ec4f785e38570902fd24e0b522b46cb171872d1ea038e88328e7d973ff47d9392f72b2d49566209eb88eb60aed8534a965cf30072c39565bd8d72f68ac';
 var Authorization ='Basic MTBkMmNiNWZjMDRjZTUxZTA2MDAwMDAxOnNoaGggdGhpcyBpcyBhIHNlY3JldA==' ;
 var oAuthParams = {
-    url: 'http://rest-proxy:4000/oauth/token',
+    url: 'http://127.0.0.1:4000/oauth/token',
     method : "POST",    
     body: 'username=oauthuser&password=oauthpassword&grant_type=password' ,     
     json: true, 
