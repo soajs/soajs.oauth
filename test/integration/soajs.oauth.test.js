173edc4bfac67a225169a4005d55bffce2643a98
mikehajj <mike.hajj@gmail.com>
2015-04-20 16:34:05 +0300

    updated tests

--- test/integration/soajs.oauth.test.js
+++ test/integration/soajs.oauth.test.js
@@ -9,7 +9,7 @@ var config = null;
 var extKey = 'aa39b5490c4a4ed0e56d7ec1232a428f771e8bb83cfcee16de14f735d0f5da587d5968ec4f785e38570902fd24e0b522b46cb171872d1ea038e88328e7d973ff47d9392f72b2d49566209eb88eb60aed8534a965cf30072c39565bd8d72f68ac';
 var Authorization = 'Basic MTBkMmNiNWZjMDRjZTUxZTA2MDAwMDAxOnNoaGggdGhpcyBpcyBhIHNlY3JldA==';
 var oAuthParams = {
-	url: 'http://127.0.0.1:4002/token',
+	url: 'http://127.0.0.1:4000/oauth/token',
 	method: "POST",
 	body: 'username=oauthuser&password=oauthpassword&grant_type=password',
 	json: true,
@@ -31,7 +31,7 @@ function executeMyRequest(params, apiPath, method, cb) {
 
 	function requester(apiName, method, params, cb) {
 		var options = {
-			uri: 'http://127.0.0.1:4002/' + apiName,
+			uri: 'http://127.0.0.1:4000/oauth/' + apiName,
 			headers: {
 				'Content-Type': 'application/json',
 				key: extKey
