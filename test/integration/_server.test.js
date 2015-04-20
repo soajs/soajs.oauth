847767adc2ec89540336fe47cd7f7f9a42b93d55
mikehajj <mike.hajj@gmail.com>
2015-04-20 16:24:47 +0300

    updated tests, coverage is at 91.3%

---
+++ test/integration/_server.test.js
@@ -0,0 +1,30 @@
+"use strict";
+var assert = require('assert');
+var shell = require('shelljs');
+var helper = require("../helper.js");
+var sampleData = require("soajs.mongodb.data/modules/oauth");
+var oauthService, controller;
+
+describe("importing sample data", function() {
+
+	it("do import", function(done) {
+		shell.pushd(sampleData.dir);
+		shell.exec("chmod +x " + sampleData.shell, function(code) {
+			assert.equal(code, 0);
+			shell.exec(sampleData.shell, function(code) {
+				assert.equal(code, 0);
+				shell.popd();
+				done();
+			});
+		});
+	});
+
+	after(function(done) {
+		console.log('test data imported.');
+		controller = require("soajs.controller");
+		oauthService = helper.requireModule('./index');
+		setTimeout(function(){
+			done();
+		}, 2000);
+	});
+});
