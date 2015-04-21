31464caaf0da403fb8582a3637bf51b59269d43f
antoinehage <me@antoinehage.com>
2015-04-20 22:30:26 -0400

    test cases PASSED

--- test/integration/_server.test.js
+++ test/integration/_server.test.js
@@ -5,26 +5,29 @@ var helper = require("../helper.js");
 var sampleData = require("soajs.mongodb.data/modules/oauth");
 var oauthService, controller;
 
-describe("importing sample data", function() {
+describe("importing sample data", function () {
 
-	it("do import", function(done) {
-		shell.pushd(sampleData.dir);
-		shell.exec("chmod +x " + sampleData.shell, function(code) {
-			assert.equal(code, 0);
-			shell.exec(sampleData.shell, function(code) {
-				assert.equal(code, 0);
-				shell.popd();
-				done();
-			});
-		});
-	});
+    it("do import", function (done) {
+        shell.pushd(sampleData.dir);
+        shell.exec("chmod +x " + sampleData.shell, function (code) {
+            assert.equal(code, 0);
+            shell.exec(sampleData.shell, function (code) {
+                assert.equal(code, 0);
+                shell.popd();
+                done();
+            });
+        });
+    });
 
-	after(function(done) {
-		console.log('test data imported.');
-		controller = require("soajs.controller");
-		oauthService = helper.requireModule('./index');
-		setTimeout(function(){
-			done();
-		}, 2000);
-	});
+    after(function (done) {
+        console.log('test data imported.');
+        controller = require("soajs.controller");
+        setTimeout(function () {
+            oauthService = helper.requireModule('./index');
+            setTimeout(function () {
+                require("./soajs.oauth.test.js");
+                done();
+            }, 1000);
+        }, 1000);
+    });
 });
