a2c0d2455a1f4ad2393bb46cf9ffa1300bc501ac
antoinehage <me@antoinehage.com>
2019-10-04 15:00:50 -0400

    added the latest importer and helper

---
+++ test/helper.js
@@ -0,0 +1,16 @@
+
+/**
+ * @license
+ * Copyright SOAJS All Rights Reserved.
+ *
+ * Use of this source code is governed by an Apache license that can be
+ * found in the LICENSE file at the root of this repository
+ */
+
+"use strict";
+
+module.exports = {
+	requireModule: function (path) {
+		return require((process.env.APP_DIR_FOR_CODE_COVERAGE || '../') + path);
+	}
+};
