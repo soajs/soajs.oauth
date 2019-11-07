5ec3a9fa0cab074b070bbddbf78e98c1aab721a2
Vinny Nordi <viniciusnordiesperanca@gmail.com>
2019-11-07 16:00:38 -0300

    Adding config file

--- passport-login/facebook/config/config.js
+++ passport-login/facebook/config/config.js
@@ -1,5 +1,5 @@
 exports.credentials = {
-	clientID: '925653057789806',
-	clientSecret: 'ad995b1ebcc1ad804517c9d5f80e0fb6',
+	clientID: '<clientID>',
+	clientSecret: '<clientSecret>',
 	callbackURL: 'http://localhost:8080/return'
 };
