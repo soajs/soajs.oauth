6903ef252b9b63fa8a153762dd41909e62c13a25
antoinehage <me@antoinehage.com>
2020-04-14 08:41:00 -0400

    enhanced integration test

--- test/integration/index.js
+++ test/integration/index.js
@@ -15,18 +15,17 @@ describe("starting OAUTH integration tests", () => {
 			if (msg) {
 				console.log(msg);
 			}
-			console.log("Starting Controller and OAUTH service");
-			controller = require("soajs.controller");
-			setTimeout(function () {
-				
+			console.log("Starting Controller ...");
+			controller = require("soajs.controller/_index.js");
+			controller.runService(() => {
+				console.log("Starting OAUTH ...");
 				service = helper.requireModule('./_index.js');
 				service.runService(() => {
 					setTimeout(function () {
 						done();
 					}, 5000);
 				});
-				
-			}, 5000);
+			});
 		});
 	});
 	
